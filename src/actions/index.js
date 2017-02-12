25ffb49fa7420b6e03156183e01b6605682a1ed4
Edouard MLT <edouard.malot@gmail.com>
2017-02-12 19:48:36 +0100

    form able to post

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,8 +4,8 @@ import axios from 'axios'
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api/';
 const API_KEY = 'edouard';
 
-export const FETCH_POST = "FETCH_POST"
-
+export const FETCH_POST = "FETCH_POST";
+export const CREATE_POST = "CREATE_POST";
 
 export function fetchPost(){
 
@@ -16,4 +16,16 @@ export function fetchPost(){
     type: FETCH_POST,
     payload: promise
   }
+}
+
+
+export function createPost(props) {
+  const URL = `${ROOT_URL}posts/?key=${API_KEY}`;
+  const promise = axios.post(URL,props);
+
+  return {
+    type: CREATE_POST,
+    payload: promise
+  }
+
 }
--- src/containers/post_new.js
+++ src/containers/post_new.js
@@ -1,17 +1,39 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/';
 
 class PostNew extends React.Component {
 
   render() {
+
+    const {fields: {title, categories, content}, handleSubmit} = this.props;
+
     return (
-        <div>
-        New post !
-            <form>
-            </form>
-        </div>
+          <form onSubmit={handleSubmit(this.props.createPost)}>
+              <h3>Create a post</h3>
+
+              <div className="from-group">
+                  <label>Title</label>
+                  <input type="text" className="form-control" {...title}/>
+              </div>
+
+              <div className="from-group">
+                  <label>Categories</label>
+                  <input type="text" className="form-control" {...categories}/>
+              </div>
+
+              <div className="from-group">
+                  <label>Content</label>
+                  <textarea className="form-control" {...content}/>
+              </div>
+
+              <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
     );
   }
 }
 
-export default connect(null, null)(PostNew);
+export default reduxForm({
+  form: 'PostNewForm',
+  fields: [ 'title', 'categories', 'content']
+}, null, { createPost })(PostNew);
